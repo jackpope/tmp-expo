d3c980ad6612944f6e2c49ab60a30d508a1cad04
Jack Pope <jackpope@meta.com>
2023-12-13 12:47:55 -0500

    f

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   setupFiles: ["@react-native/test-renderer/jest-setup"],
   testEnvironment: "@react-native/test-renderer/jest-environment",
-  preset: "jest-expo",
+  haste: {
+    defaultPlatform: 'ios',
+    platforms: ['android', 'ios', 'native'],
+  },
+  // preset: "jest-expo",
   transformIgnorePatterns: [
     "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)",
   ],
--- src/components/__tests__/HelloWorld-test.js
+++ src/components/__tests__/HelloWorld-test.js
@@ -1,8 +1,18 @@
-import {render} from '@react-native/test-renderer';
+import {ReactNativeTestRenderer} from '@react-native/test-renderer';
+import {View, Text} from 'react-native';
+
+function TestComponent() {
+  return (
+    <View>
+      <Text>Hello</Text>
+      <View />
+    </View>
+  );
+}
 
 describe('snapshot example', () => {
-  it('returns expected JSON output based on renderer component', async () => {
-    const result = await render(<TestComponent />);
-    expect(result.toJSON()).toMatchSnapshot();
+  it('returns expected JSON output based on renderer component', () => {
+    const result = ReactNativeTestRenderer.render(<TestComponent />)
+    expect(result.toJSON()).toMatchSnapshot()
   }, 100000);
 });
