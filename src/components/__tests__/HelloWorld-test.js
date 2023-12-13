e8a0737d8e4ce75ae1ce76b0880ab6c51b760f7e
Jack Pope <jackpope@meta.com>
2023-12-13 16:14:46 -0500

    Add additional tests

--- src/components/__tests__/HelloWorld-test.js
+++ src/components/__tests__/HelloWorld-test.js
@@ -1,27 +1,45 @@
 import {ReactNativeTestRenderer} from '@react-native/test-renderer';
-import {View, Text} from 'react-native';
 import * as React from 'react';
 import * as ReactTestRenderer from 'react-test-renderer'
-
-function TestComponent() {
-  return (
-    <View>
-      <Text>Hello</Text>
-      <View />
-    </View>
-  );
-}
+import HelloWorld from '../HelloWorld';
+import { View, Text } from 'react-native';
 
 describe('rtr', () => {
   it('returns expected JSON output based on renderer component', () => {
-    const result = ReactTestRenderer.create(<TestComponent />)
+    const result = ReactTestRenderer.create(<HelloWorld />)
     expect(result.toJSON()).toMatchSnapshot()
   });
 })
 
-describe('react-native-test-renderer', () => {
-  it('returns expected JSON output based on renderer component', () => {
-    const result = ReactNativeTestRenderer.render(<TestComponent />)
-    expect(result.toJSON()).toMatchSnapshot()
+describe('render', () => {
+  describe('toJSON', () => {
+    it('returns expected JSON output based on renderer component', () => {
+      const result = ReactNativeTestRenderer.render(<HelloWorld />);
+      expect(result.toJSON()).toMatchSnapshot();
+    });
+  });
+
+  describe('findByTestID', () => {
+    it('returns the first element with a matching testID', () => {
+      const result = ReactNativeTestRenderer.render(<HelloWorld />);
+      expect(result.findByTestID('world-container')).not.toBeNull();
+    });
+  });
+
+  describe('press', () => {
+    it('invokes onPress callback', () => {
+      let pressed = false;
+      const result = ReactNativeTestRenderer.render(
+        <View>
+          <Text testID="text" onPress={() => (pressed = true)}>
+            Hello
+          </Text>
+        </View>,
+      );
+      const text = result.findByTestID('text');
+      expect(pressed).toBe(false);
+      ReactNativeTestRenderer.fireEvent(text, 'press');
+      expect(pressed).toBe(true);
+    });
   });
 });
