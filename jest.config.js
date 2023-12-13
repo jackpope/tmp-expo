208a04c09edf865e7a6664abe09b0df686d57da7
Jack Pope <jackpope@meta.com>
2023-12-13 16:09:29 -0500

    test passes

--- jest.config.js
+++ jest.config.js
@@ -9,4 +9,7 @@ module.exports = {
   transformIgnorePatterns: [
     "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)",
   ],
+  moduleNameMapper: {
+    "^react($|/.+)": "<rootDir>/node_modules/react$1"
+  },
 };
