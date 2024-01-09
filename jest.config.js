24aa2861369d5b24e735060bc1c15da7de652b3f
Jack Pope <jackpope@meta.com>
2024-01-09 15:54:39 -0500

    remove jest-expo preset

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,6 @@ module.exports = {
     defaultPlatform: 'ios',
     platforms: ['android', 'ios', 'native'],
   },
-  // preset: "jest-expo",
   transformIgnorePatterns: [
     "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)",
   ],
